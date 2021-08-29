0c52388ad8daa18f7cc73a7048943cbdf41a6855
Paulo Victor <pauloaraujo2607@gmail.com>
2021-08-29 04:52:12 -0400

    Commit inicial

--- App.js
+++ App.js
@@ -1,11 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import { Picker } from '@react-native-picker/picker'
+import { TextInputMask } from 'react-native-masked-text'
+ 
 export default function App() {
+  function handleSubmit() {
+    // const calc = ((watts * days * hour) / 1000) * price
+    // alert(calc)
+    const unmask = priceRef?.current.getRawValue
+    alert(unmask)
+  }
+
+  const [watts, setWatts] = useState('');
+  const [days, setDay] = useState('')
+  const [price, setPrice] = useState('')
+
+  function handleSubmit() {
+    alert('Você clicou, parabéns!')
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.inputs}>
+        <TextInput
+        style={styles.textInput}
+        value={watts}
+        onChangeText= { (watts) => setWatts(watts) }
+        placeholder="Watts"
+        keyboardType="numeric" />
+      </View>
+
+      <View style={styles.inputs}>
+        <TextInput
+        style={styles.textInput}
+        value={days}
+        onChangeText= { (days) => setDay(days) }
+        placeholder="Dias"
+        keyboardType="numeric" />
+
+        <TextInputMask
+        style={styles.textInput}
+        type={'money'}
+        value={price}
+        onChangeText= { (price) => setPrice(price) }
+        placeholder="Preço (KWh)"
+        keyboardType="numeric" 
+        />
+      </View>
+
+      <TouchableOpacity
+          style={styles.button}
+          onPress={handleSubmit}>
+          <Text style={styles.buttonText}>Calcular</Text>
+        </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +63,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#F5FCFF',
   },
+
+  inputs: {
+    flexDirection: 'row'
+  },
+
+  textInput: {
+    width: '43%',
+    fontSize: 25,
+    marginTop: 50,
+    textAlign: 'center'
+  },
+
+  pickers: {
+    height: '43%',
+    width: '27%',
+    marginTop: 50
+  },
+
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 15,
+    backgroundColor: '#41Aef4',
+    padding: 10,
+    color: '#FFF',
+    borderRadius: 100
+  },
+
+  buttonText: {
+    color: '#FFF',
+    fontSize: 17
+  }
+
 });
---
+++ README.md
@@ -0,0 +1,7 @@
+Este é um aplicativo que calcula o quanto o aparelho X consome em X dias
+escolhido pelo usuário com base nas infomações dadas pelo mesmo.
+
+Aplicativo simples e sendo desenvolvido por um estudante do framework
+React Native!
+
+Qualquer melhoria, sugestão e/ou conselho, será bem vindo! :)
